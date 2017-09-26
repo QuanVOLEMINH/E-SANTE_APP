6d84d9a83e6f04e6db6b5745ad1bd244db99a6af
QuanVOLEMINH <wazzamq1104@gmail.com>
2017-09-26 17:14:54 +0200

    modify interface

--- MyIonicProject/src/providers/app-settings/app-settings.ts
+++ MyIonicProject/src/providers/app-settings/app-settings.ts
@@ -3,7 +3,7 @@ import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
 
 const CONFIG = {
-  apiUrl: 'http://127.0.0.1:3001/'
+  apiUrl: 'http://192.168.1.113:3001/'
   //debug on laptop: 127.0.0.1 (localhost)
   //on mobile: connect to the same wifi -> use laptop's ip address in THIS WIFI (192.168.1.113)
 }
