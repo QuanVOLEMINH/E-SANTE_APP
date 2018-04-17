8fac77a0225b565d10340e7aab422835a0aed902
QuanVOLEMINH <quanvo1104@gmail.com>
2018-04-17 22:23:09 +0200

    'configuration

--- MyIonicProject/src/providers/app-settings/app-settings.ts
+++ MyIonicProject/src/providers/app-settings/app-settings.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import 'rxjs/add/operator/map';
 
 const CONFIG = {
-  apiUrl: 'http://192.168.1.113:3001/'
+  apiUrl: 'http://192.168.1.128:3001/'
   //debug on laptop: 127.0.0.1 (localhost)
   //on mobile: connect to the same wifi -> use laptop's ip address in THIS WIFI (192.168.1.113)
 }
